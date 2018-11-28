30e6afbd95913795dcc32db7f24287fb7c06efed
Kamry Bowman <kamry.bowman@gmail.com>
2018-11-28 15:56:41 -0700

    Implement login route

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const db = require('knex')(require('./knexfile').development);
 const morgan = require('morgan');
@@ -13,6 +14,7 @@ server.post('/api/register', (req, res) => {
   if (!username || !password || !department ) {
     return res.status(400).json({ message: 'Include all information' });
   }
+ 
   const hash = bcrypt.hashSync(password, 10); //shortened for development
   return db('users')
     .insert({ username, hash, department })
@@ -26,12 +28,31 @@ server.post('/api/register', (req, res) => {
 });
 
 function generateToken(user) {
-
+  const { username, department, userId } = user;
+  const options = {
+    expiresIn: '1h',
+  };
+  return jwt.sign({ username, department, userId }, process.env.SECRET, options);
 }
 
 server.post('/api/login', (req, res) => {
-  const { name, password } = req.body;
-  
+  const { username, password, } = req.body;
+  if (!username || !password ) {
+    return res.status(400).json({ message: 'Include all information' });
+  }
+
+  return db('users').where('username', username).first()
+    .then(({ username, hash, department, id }) => {
+      if (username && bcrypt.compareSync(password, hash)) {
+        const token = generateToken({ username, department, userId: id });
+        return res.status(200).json({ id, username, department, token });
+      }
+      return res.status(401).json({ message: 'Failed to authenticate' });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({ err });
+    });
 });
 
 // server.get('/api/restricted/users', jwt({ secret: 'kam-secret-007' }), (req, res) => {
