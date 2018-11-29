9a8420a32c31330c1a05070e77e0993b62c1f408
Kamry Bowman <kamry.bowman@gmail.com>
2018-11-29 11:37:11 -0700

    Implement login with token

--- lambda-client/src/App.js
+++ lambda-client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Register from './components/Register';
-import { Switch, Route } from 'react-router-dom';
+import Login from './components/Login';
+import { Switch, Route, NavLink } from 'react-router-dom';
 import './App.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
@@ -8,9 +9,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/login">Login</NavLink>
+          <NavLink to="/register">Register</NavLink>
+        </nav>
         <section>
           <Switch>
             <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
           </Switch>
         </section>
       </div>
